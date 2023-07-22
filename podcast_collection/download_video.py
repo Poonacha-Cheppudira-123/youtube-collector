7c20472d4c96e3b52190ebb09aa23d16b2ffe1f7
Poonacha Cheppudira <poonachausa@gmail.com>
2023-07-21 17:14:39 -0700

    Created process to download urls

---
+++ podcast_collection/download_video.py
@@ -0,0 +1,49 @@
+import yt_dlp
+
+
+def download_youtube(
+    url: str, video_title: str, rate_limit=5000000, my_format="best[ext=mp4]"
+) -> str:
+    """
+    Download a YouTube video, channel, or playlist based on the provided URL.
+
+    Parameters:
+        url (str): The URL of the YouTube video, channel, or playlist to be downloaded.
+        video_title (str): The title of the YouTube video. Used when downloading a single video.
+        rate_limit (int, optional): The download rate limit in bytes per second. Default is 5000000 (5 MB/s).
+        my_format (str, optional): The format of the downloaded video. Default is 'best[ext=mp4]'.
+
+    Returns:
+        str: The file path where the video or channel/playlist is downloaded.
+    """
+
+    file_path = "C:/Users/azs547/Code/cmo-podcast-collection/"
+    ydl_opts = None
+
+    if url.startswith("https://www.youtube.com/@"):
+        ydl_opts = {
+            "ignoreerrors": True,
+            "abort_on_unavailable_fragments": True,
+            "format": my_format,
+            "outtmpl": file_path
+            + "/Channels/%(uploader)s/%(title)s ## %(uploader)s ## %(id)s.%(ext)s",
+            "ratelimit": rate_limit,
+        }
+    elif url.startswith(
+        "https://www.youtube.com/watch",
+    ):
+        formatted_title = video_title.replace(" ", "_")
+        file_path += formatted_title
+        ydl_opts = {
+            "ignoreerrors": True,
+            "abort_on_unavailable_fragments": True,
+            "format": my_format,
+            "outtmpl": file_path + ".%(ext)s",
+            "ratelimit": rate_limit,
+        }
+
+    if ydl_opts is not None:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(url)
+
+    return file_path
